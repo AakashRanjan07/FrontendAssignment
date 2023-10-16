4c6b9c999348ca428da03fdf5be96bf11481a912
AakashRanjan07 <92005585+AakashRanjan07@users.noreply.github.com>
2023-10-16 18:22:00 +0530

    ReactAssignment

---
+++ Reactcomponent/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+  //      const { useState } = React; 
+
+  // const [count, setCount] = useState(0); 
+
+ 
+    <div className="app">
+      {
+        // if else statement to determine color of the counter
+      }
+      <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>
+        {count}
+      </h1>
+      <div className="button__wrapper">
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
