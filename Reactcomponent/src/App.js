36e6b83014490a378db39a5774c30d39891e3772
AakashRanjan07 <92005585+AakashRanjan07@users.noreply.github.com>
2023-10-20 15:18:18 +0530

    App.js

--- Reactcomponent/src/App.js
+++ Reactcomponent/src/App.js
@@ -4,15 +4,13 @@ import './App.css';
 function App() {
   return (
    
-  //      const { useState } = React; 
+    const { useState } = React; 
 
-  // const [count, setCount] = useState(0); 
+  const [count, setCount] = useState(0); 
 
  
     <div className="app">
-      {
-        // if else statement to determine color of the counter
-      }
+    
       <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>
         {count}
       </h1>
